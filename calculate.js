ff101b4f8383f0a128263ddc67fca178cb331ef6
sujal-kalvani <160718331+sujal-kalvani@users.noreply.github.com>
2024-02-21 14:57:15 +0530

    Add files via upload

---
+++ calculate.js
@@ -0,0 +1,44 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let input = document.getElementById('inputBox');
+    let buttons = document.querySelectorAll('button');
+
+    let string = "";
+    let arr = Array.from(buttons);
+
+    arr.forEach(button => {
+        button.addEventListener('click', (e) => {
+            if (e.target.textContent == '=')
+             {
+                try {
+                    string = eval(string);
+                    input.value = string;
+                } catch (error) {
+                    input.value = 'Error';
+                }
+            } else if (e.target.textContent == 'c') 
+            {
+                string = "";
+                input.value = string;
+            } else if (e.target.textContent == 'DEL') 
+            {
+                string = string.substring(0, string.length - 1);
+                input.value = string;
+            } 
+            else if(e.target.textContent=="x")
+            {
+                string = string+"*";
+                input.value = string;
+            }
+            else if(e.target.textContent=="÷")
+            {
+                string = string+"/";
+                input.value = string;
+            }
+            else 
+            {
+                string += e.target.textContent;
+                input.value = string;
+            }
+        });
+    });
+});
